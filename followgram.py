4a0febc470dd459898315a4aa4b17b4a480b25a1
z3ro0fc <99695665+z3ro0fc@users.noreply.github.com>
2022-02-15 17:45:06 -0300

    Create followgram.py

---
+++ followgram.py
@@ -0,0 +1,57 @@
+from colorama import Fore, init
+init()
+#$$
+rojo = Fore.RED
+violeta = Fore.MAGENTA
+azul = Fore.BLUE
+cyan = Fore.CYAN
+verde = Fore.GREEN
+
+#portada
+
+print(f"""
+
+{violeta} 
+░██████╗░██████╗░░█████╗░███╗░░░███╗
+██╔════╝░██╔══██╗██╔══██╗████╗░████║
+██║░░██╗░██████╔╝███████║██╔████╔██║
+██║░░╚██╗██╔══██╗██╔══██║██║╚██╔╝██║
+╚██████╔╝██║░░██║██║░░██║██║░╚═╝░██║
+░╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░░░╚═╝ {violeta} """)
+
+#follow gram
+follow = input("coloca el usuario // ")
+if not True:
+ print('usuario incorrecto')
+else:
+  print(f"""
+  
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK098{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK099{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK100{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK101{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK102{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK103{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK104{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK105{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK106{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK107{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK108{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK109{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK110{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK111{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK112{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK113{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK114{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK115{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK116{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK117{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK118{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK119{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK120{rojo}
+{cyan}[{cyan}{verde}+{cyan}]{cyan}{verde} Following{verde}{rojo}    =   {rojo} {cyan}Numero de acceso {cyan} {rojo} HK121{rojo}
+
+{rojo} $$$$$ Created by Z3RO $$$$$ La herramienta tarda unos 7 dias en colocar totalmente los 7000 bots ya que deben programarse automaticamente y deben ser seguros para evitar un ban de ka cuenta :)
+ """)
+  
+  
