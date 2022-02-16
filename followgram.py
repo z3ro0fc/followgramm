aed1582f7a51520649b6ddd88b8059028708f57c
Z3RO <99695665+z3ro0fc@users.noreply.github.com>
2022-02-16 14:05:22 -0300

    Update followgram.py

--- followgram.py
+++ followgram.py
@@ -25,60 +25,14 @@ print(f"""
 {violeta} Version 2.0 {violeta}
 {verde} telegram : @z3roofc""")
 
-def seguidores():
-    follow = input("coloca el usuario // ")
-if not True:
-    print('usuario incorrecto')
-else:
-  for i in range(1000):
-      print(f"""{cyan}[{cyan} {amarillo}+{amarillo}{cyan}]{cyan}{verde} El bot se esta programando tardara 20 dias{verde}""")
-      
-      
+
       
   
+     #followgram
      
-def fuerzabruta():
-      password = input("coloca el correo")
+seguir = input("coloca el usuario === ")
 if not True:
-           print("La contraseña tuene mas de 10 caracteres")
+    print("Esta mal")
 else:
-  if __name__ == '__main__':
- 
-    rstr = random.choice(string.ascii_letters)
-    print(rstr)
-    
-    #menu
-  
-def menu():
-      print(""" - follow : da seguidores
-      - fuerzagram : obtiene la cintraseña de la cuenta""")
-      
-     #eleccion
-     
-      
-def eleccion():
-    print(f"{verde}")
-    opc = input(f"[followgram@root]>> ")
-    if opc == "follow":
-        seguidores()
-        eleccion()
-    elif opc == "help":
-        menu();
-        eleccion()
-    elif opc == "fuerzagram":
-       fuerzabruta()
-       eleccion()
-    else:
-        print(f"""{rojo}
-    ERROR 404 OPCIÓN INCORRECTA x_x 
-        {verde}""")
-        eleccion()
-    
-# inicio de tool
-if __name__ == "__main__":
-    eleccion()
-      
-      
-      
-      
-    
+    for i in range(1000000000):
+        print(f"""{rojo}[{violeta}+{rojo}]{cyan} Los bots se le estan ejecutando al usuario tardara un par de dias""")
